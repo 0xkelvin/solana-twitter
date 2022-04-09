0447fb2b8842c646c24837767960ebed63e5c7dc
Kelvin <quocvm1@gmail.com>
2022-04-09 20:21:51 +0800

    update program id

--- programs/solana-twitter/src/lib.rs
+++ programs/solana-twitter/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("EGGC7zME9gqRmGLP2UrrBKdjbCterC6cz2RXfwFtQyxg");
 
 #[program]
 pub mod solana_twitter {
